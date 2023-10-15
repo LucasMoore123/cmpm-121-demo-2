f0a0e9833e5861b42b652abb9e33d22b4074545e
LucasMoore123 <luamoore@ucsc.edu>
2023-10-15 11:48:11 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.backgroundColor = "white";
+canvas.style.border = "2px solid black";
+canvas.style.borderRadius = "15px";
+canvas.style.boxShadow = "10px 10px 10px rgba(220, 198, 255, 0.7)";
+app.append(canvas);
